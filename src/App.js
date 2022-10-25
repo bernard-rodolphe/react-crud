8f887122cc6294ea8c807db5f2ddc892cff64b3a
Roro BERNARD <mr.rodolphe.bernard@gmail.com>
2022-10-25 10:10:37 +0200

    Ajout car.js, truck.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Truck from './Truck';
+import Car from './Car';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from react.</h1>
+      <Car/>
+      <Truck/>
     </div>
   );
 }
---
+++ src/Truck.js
@@ -0,0 +1,5 @@
+function Truck(){
+    return <h2> Je suis un camion. </h2>
+}
+
+export default Truck;
